a6c1d6da6fda2132d9d8cff992deb48cd21aa790
Saratina878 <109515581+Saratina878@users.noreply.github.com>
2022-08-08 22:25:55 +0700

    add edit and remove lecturer

---
+++ resources/views/edit-lecturer.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Edit Lecturer</title>
+</head>
+<body>
+@if(Session::has('lecturer_edit'))
+<div class="alert alert-success" role="alert">{{Session::get('lecturer_edit')}} </div>
+
+@endif
+
+  <form method="POST" action="{{url('update-lecturer')}}">
+    @csrf
+    <input type="hidden" name="id" value="{{$lecturers->id}}">
+     Full Name: <br><input type="text" name="fullName" value="{{$lecturers->fullName}}"><br>
+     Email <br><input type="text" name="email" value="{{$lecturers->email}}"><br>
+     Phone <br><input type="text" name="phone" value="{{$lecturers->phone}}"><br>
+     <label for="major">Choose a major:</label>
+    <select name="major_id" id="majors">
+        {{-- <!-- <option value="{{$major_id}}"> {{$major->majorName}} </option> --> --}}
+        @foreach ($majors as $major)
+          <option value="{{$major->id}}">{{$major->majorName}} </option>  
+        @endforeach
+</select>
+<label for="classroom">Choose a classroom:</label>
+    <select name="classroom_id" id="classrooms">
+  {{-- <!-- <option value="{{$major_id}}"> {{$major->majorName}} </option> --> --}}
+  @foreach ($classrooms as $classroom)
+    <option value="{{$classroom->id}}">{{$classroom->classCode}} </option>  
+  @endforeach
+</select>
+    <input type="submit" value="Submit">
+  </form>
+
+
+
+</body>
+</html>
--- resources/views/list-lecturer.blade.php
+++ resources/views/list-lecturer.blade.php
@@ -1,36 +1,66 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Lecturer List</title>
-</head>
-<body>
-  <table>
-    <tr>
-    <th>Lecturer Code</th> 
-    <th>Full Name</th>
-    <th>Password</th>
-    <th>Email</th>
-    <th>Phone</th>
-    <th>Major</th>
-    </tr>  
- 
-      @foreach($lecturers as $lecturer)
-      <tr>
-          <td>{{$lecturer->lecturerCode}}
-          </td>
-          <td>{{$lecturer->fullName}}</td>
-          <td>{{$lecturer->password}}</td>
-          <td>{{$lecturer->email}}</td>
-          <td>{{$lecturer->phone}}</td>
-          <td>{{$lecturer->major->majorName}}</td>
-      </tr>
-      @endforeach
-  
-  </table> 
+=@extends('pages.adminlayout')
+
+@section('content')
+
+<main role="main" class="main-content">
+  @if(Session::has('lecturer_list'))
 
 
-</body>
-</html>
+<div class="alert alert-success" role="alert">{{Session::get('lecturer_list')}} </div>
+@endif
+
+    <div class="container-fluid">
+      <div class="row justify-content-center">
+        <div class="col-12">
+          <h2 class="mb-2 page-title">Lecturer List</h2>
+          
+          <div class="row my-4">
+            <!-- Small table -->
+            <div class="col-md-12">
+              <div class="card shadow">
+                <div class="card-body">
+                  <!-- table -->
+                  <table class="table datatables" id="dataTable-1">
+                    <thead>
+                      <tr>
+                        <th>Lecturer Code</th>
+                        <th>Full Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Major</th>
+                        <th>Class code</th>
+                        <th>Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      @foreach ($lecturers as $lecturer)
+                      <tr>
+                       <td>{{$lecturer->lecturerCode}}</td>
+                        <td>{{$lecturer->fullName}}</td>
+                        <td>{{$lecturer->email}}</td>
+                        <td>{{$lecturer->phone}}</td>
+                        <td>{{$lecturer->major->majorName}}</td>
+                        <td>{{$lecturer->classroom !== null ?  $lecturer->classroom->classCode : "Not yet"}}</td>
+                        <td><button class="btn btn-sm dropdown-toggle more-horizontal" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span class="text-muted sr-only">Action</span>
+                          </button>
+                          <div class="dropdown-menu dropdown-menu-right">
+                            <a class="dropdown-item" href="{{url('edit-lecturer/'.$lecturer->id)}}">Edit</a>
+                            <a class="dropdown-item" href="{{url('remove-lecturer/'.$lecturer->id)}}">Remove</a>
+                            <a class="dropdown-item" href="#">Assign</a>
+                          </div>
+                        </td>
+                      </tr>
+                      @endforeach
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div> <!-- simple table -->
+          </div> <!-- end section -->
+        </div> <!-- .col-12 -->
+      </div> <!-- .row -->
+    </div> <!-- .container-fluid -->
+    
+  </main> <!-- main -->
+  @endsection
